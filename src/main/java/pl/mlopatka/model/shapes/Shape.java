fe065f1537e068d97c8585b59876178a3af6a485
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-05 13:32:56 +0200

    Add color to shape

--- src/main/java/pl/mlopatka/model/shapes/Shape.java
+++ src/main/java/pl/mlopatka/model/shapes/Shape.java
@@ -1,9 +1,12 @@
 package pl.mlopatka.model.shapes;
 
+import pl.mlopatka.model.colors.Color;
+
 /**
  * Created by marcin.lopatka on 05-07-2019
  */
 public class Shape {
 	
 	private double field;
+	private Color color;
 }
