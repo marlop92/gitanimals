3696d334d3c9f0602765238df77a437fd38bca0d
marcin.lopatka <marcin.lopatka@mercareon.com>
2019-07-05 12:07:33 +0200

    Initial commit

---
+++ src/main/java/pl/mlopatka/Main.java
@@ -0,0 +1,11 @@
+package pl.mlopatka;
+
+/**
+ * Created by marcin.lopatka on 05-07-2019
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
